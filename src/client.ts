817ae8dfe750b46f226d71f8eff5e707eed57744
pcollinsonline <pcollinsonline@gmail.com>
2023-10-24 11:31:01 -0400

    chore: upgrade houdini to v1

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,10 @@
-import { HoudiniClient, type RequestHandler } from '$houdini'
+import { HoudiniClient } from '$houdini'
 
 import { PUBLIC_DATOCMS_API_TOKEN } from '$env/static/public'
 
-const requestHandler: RequestHandler = async ({ fetch, text = '', variables = {} }) => {
-	const url = 'https://graphql.datocms.com/'
-	//           ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑
+export default new HoudiniClient({
+	url: 'https://graphql.datocms.com/',
+	//     ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑
 	// Make sure that GraphQL API URL declared here looks exactly the same as
 	// the one the SvelteKit uses for storing hydrating data in HTML:
 	// by inspecting a page rendered by SvelteKit, you'll find a tag like
@@ -16,19 +16,14 @@ const requestHandler: RequestHandler = async ({ fetch, text = '', variables = {}
 	// `data-url` attribute must have the same exact value used in this file. Otherwise,
 	// some useless API calls will be done at each page load.
 
-	const result = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${PUBLIC_DATOCMS_API_TOKEN}`
-		},
-		body: JSON.stringify({
-			query: text,
-			variables
-		})
-	})
-
-	return await result.json()
-}
-
-export default new HoudiniClient(requestHandler)
+	fetchParams: ({ variables, text }) => {
+		return {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${PUBLIC_DATOCMS_API_TOKEN}`
+			},
+			body: JSON.stringify({ query: text, variables })
+		}
+	}
+})
